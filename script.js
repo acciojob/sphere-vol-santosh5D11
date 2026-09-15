4d33a179e73f578c605d8d1a028bfa756e24040f
Motapandle Santosh <santoshmotapandle@gmail.com>
2026-09-15 23:22:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function volume_sphere() {
-    //Write your code here
-  
+	var radius = parseFloat(document.getElementById('radius').value);
+	var volume = document.getElementById('volume');
+
+	if(isNaN(radius) || radius < 0){
+		volume.value = "NaN";
+	}else{
+		volume.value = ((4/3) * Math.PI * Math.pow(radius,3)); 
+	}
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
